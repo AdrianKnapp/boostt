d4ac4d3e34a5fbb2b13a647e2908808a37309135
Adrian Knapp <adrknapp@gmail.com>
2022-08-02 19:08:13 -0300

    Adjusted benefits

--- src/components/Benefit/index.tsx
+++ src/components/Benefit/index.tsx
@@ -16,21 +16,14 @@ const Benefit = ({ title, description, icon, image }: BenefitProps) => {
   if (!image || !title) return null;
 
   return (
-    <Flex
-      width="100%"
-      _even={{ flexDirection: ['column', 'column', 'row-reverse'] }}
-      direction={['column', 'column', 'row']}
-      gap={[10, 10, 5, 0]}
-    >
-      <>
-        <Box w={['100%', '100%', '50%']}>
-          <Image image={image} />
-        </Box>
-        <Box w={['100%', '100%', '50%']}>
-          <Content title={title} description={description} icon={icon} />
-        </Box>
-      </>
-    </Flex>
+    <>
+      <Box w={['100%', '100%', '50%']}>
+        <Image image={image} />
+      </Box>
+      <Box w={['100%', '100%', '50%']}>
+        <Content title={title} description={description} icon={icon} />
+      </Box>
+    </>
   );
 };
 
