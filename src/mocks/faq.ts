f5ca4a8a4c5e3fc602534feab9f61c96a1bfdb4b
Adrian Knapp <adrknapp@gmail.com>
2022-08-07 17:18:07 -0300

    Faq improvments

--- src/mocks/faq.ts
+++ src/mocks/faq.ts
@@ -1,7 +1,7 @@
 const faqData = [
   {
     title: 'O pagamento é seguro?',
-    description: 'Sim, o pagamento é feito através da Eduzz e é 100%.',
+    description: 'Sim, o pagamento é feito através da Eduzz e é 100% seguro.',
   },
   {
     title: 'Por que a Rocket Wins é a melhor?',
@@ -10,18 +10,17 @@ const faqData = [
   },
   {
     title: 'Quais as formas de pagamento?',
-    description:
-      'Em até 12x no cartão de crédito, ou no PIX ou boleto à vista.',
+    description: 'Em até 12x no cartão de crédito, PIX ou boleto à vista.',
   },
   {
     title: 'Ao comprar, vou ter garantia?',
     description:
-      'Sim! Nós confiamos tanto no nosso produto que iremos te dar 7 dias de garantia para você testá-lo.',
+      'Sim. Nós confiamos tanto no nosso produto que iremos te dar 7 dias de garantia para você testá-lo.',
   },
   {
     title: 'A Rocket Wins funciona pelo celular?',
     description:
-      'Sim! Ela foi desenvolvida para funcionar no celular, e no desktop.',
+      'Sim. Ela foi desenvolvida para funcionar no celular e desktop.',
   },
 ];
 
