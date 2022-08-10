7bfdece726e623bb0bbec352f843e727f1153629
Adrian Knapp <adrknapp@gmail.com>
2022-08-10 11:00:12 -0300

    Updated faq

--- src/mocks/faq.ts
+++ src/mocks/faq.ts
@@ -1,26 +1,27 @@
 const faqData = [
   {
     title: 'O pagamento é seguro?',
-    description: 'Sim, o pagamento é feito através da Eduzz e é 100% seguro.',
+    description:
+      'SIM. Utilizando uma empresa independente para a intermediação dos pagamentos, a Kiwify, conseguimos garantir  100% de segurança ao cliente.',
   },
   {
-    title: 'Por que a Rocket Wins é a melhor?',
+    title: 'Por que escolher a Rocket Wins?',
     description:
-      'Porque ela é validada e proporciona lucro para milhares de pessoas espalhadas pelo Brasil. Justamente por isso, ela é a única capaz de te proporcionar os melhores resultados.',
+      'Tendo como foco principal a assertividade e o constante desenvolvimento, a Rocket wins é uma das poucas empresas com equipe própria de programadores e desenvolvedores. Isso resulta em constantes atualizações do nosso método, que se traduz em resultados positivos para você, nosso cliente.',
   },
   {
     title: 'Quais as formas de pagamento?',
-    description: 'Em até 12x no cartão de crédito, PIX ou boleto à vista.',
+    description: 'PIX, boleto bancário ou em até 12x no cartão de crédito.',
   },
   {
-    title: 'Ao comprar, vou ter garantia?',
+    title: 'Quais garantias tenho ao comprar?',
     description:
-      'Sim. Nós confiamos tanto no nosso produto que iremos te dar 7 dias de garantia para você testá-lo.',
+      'A maior garantia que podemos te oferecer são nossos resultados, os números estão aí e não mentem. Mas se mesmo assim você ainda está receoso, conte com nossa política de teste, onde você tem 7 dias para conhecer nosso produto.',
   },
   {
-    title: 'A Rocket Wins funciona pelo celular?',
+    title: 'A Rocket wins funciona pelo celular?',
     description:
-      'Sim. Ela foi desenvolvida para funcionar no celular e desktop.',
+      'Em pleno século XXI, não poderíamos deixar de oferecer acesso mobile para os nossos clientes. Os desktops ainda estão aí, mas sejamos honestos: Quem está na sua mão agora?.',
   },
 ];
 
